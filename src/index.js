5ec64e4cf334303f78f11372cb6606d349b6b1cf
Xavier Via <fernando.via@gmail.com>
2017-08-05 23:40:40 +0200

    Fix another export

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import * as _Html from './primitives/Html'
 import * as _Svg from './primitives/Svg'
-export { default as createElementWithProps } from './createElementWithProps'
+
 export { default as ReactDream } from './ReactDream'
-export { default as withStyleFromProps } from './higherOrderComponents/withStyleFromProps'
+
+export { default as createElementWithProps } from './createElementWithProps'
+
+export { default as styleFromProps } from './styleFromProps'
+
+export { default as withStyleFromProps } from './withStyleFromProps'
+
 export { default as contramap } from './polyfills/contramap'
 
 export const Html = _Html
