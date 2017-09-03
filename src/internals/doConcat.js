cca72890aaa283696f294593b9bb4de28d546548
Xavier Via <fernando.via@gmail.com>
2017-09-03 23:52:56 +0200

    Cleanup .concat implementation

--- src/internals/doConcat.js
+++ src/internals/doConcat.js
@@ -1,16 +1,9 @@
 import React from 'react'
 import getDisplayName from 'recompose/getDisplayName'
-import isReferentiallyTransparentFunctionComponent from 'recompose/isReferentiallyTransparentFunctionComponent'
 import setDisplayName from 'recompose/setDisplayName'
 
 // doConcat : Component -> Component -> Component
-export default ComponentA => ComponentB => {
-  if (
-    isReferentiallyTransparentFunctionComponent(ComponentA) &&
-    isReferentiallyTransparentFunctionComponent(ComponentB)
-  ) {
-    return setDisplayName(
-      [ComponentB, ComponentA].map(getDisplayName).filter(s => s != null && s !== '').join(' + ')
-    )(props => [<ComponentB key={0} {...props} />, <ComponentA key={1} {...props} />])
-  }
-}
+export default ComponentA => ComponentB =>
+  setDisplayName(
+    [ComponentB, ComponentA].map(getDisplayName).filter(s => s != null && s !== '').join(' + ')
+  )(props => [<ComponentB key={0} {...props} />, <ComponentA key={1} {...props} />])
