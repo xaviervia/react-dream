6fc0229c7f5c18635861dbf7862dceadbd48226b
Xavier Via <fernando.via@gmail.com>
2018-06-09 19:22:20 +0200

    Expose ReactDream as the entrypoint function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import _ReactDream from './ReactDream'
+import ReactDream from './ReactDream'
 
 export { default as createElementWithProps } from './createElementWithProps'
 
@@ -24,5 +24,6 @@ export { default as scale } from './partialApplication/scale'
 export { default as style } from './partialApplication/style'
 export { default as translate } from './partialApplication/translate'
 
-export const ReactDream = _ReactDream
-export const of = _ReactDream
+export const of = ReactDream
+
+export default ReactDream
